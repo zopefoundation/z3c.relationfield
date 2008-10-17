08d23a10888cba107681124222ecee8ae214fe46
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-17 15:20:53 +0000

    Use absolute imports, not relative ones.

--- src/z3c/relationfield/__init__.py
+++ src/z3c/relationfield/__init__.py
@@ -1,5 +1,5 @@
-from relation import RelationValue, TemporaryRelationValue
-from index import RelationCatalog
-from schema import Relation
-from event import realize_relations
-from widget import RelationWidget, RelationDisplayWidget
+from z3c.relationfield.relation import RelationValue, TemporaryRelationValue
+from z3c.relationfield.index import RelationCatalog
+from z3c.relationfield.schema import Relation
+from z3c.relationfield.event import realize_relations
+from z3c.relationfield.widget import RelationWidget, RelationDisplayWidget
