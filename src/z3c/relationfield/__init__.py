1a67404084c402427d355153a6b96cd7fe111148
faassen <faassen@62d5b8a3-27da-0310-9561-8e5933582275>
2008-10-17 15:59:56 +0000

    Cut out widget code, this moved to z3c.relationfieldui.

--- src/z3c/relationfield/__init__.py
+++ src/z3c/relationfield/__init__.py
@@ -2,4 +2,3 @@ from z3c.relationfield.relation import RelationValue, TemporaryRelationValue
 from z3c.relationfield.index import RelationCatalog
 from z3c.relationfield.schema import Relation
 from z3c.relationfield.event import realize_relations
-from z3c.relationfield.widget import RelationWidget, RelationDisplayWidget
