101624a79cec7e9e8bb87c6407aa38f2320c8d77
optilude <optilude@62d5b8a3-27da-0310-9561-8e5933582275>
2009-10-11 10:25:00 +0000

    Back to development: 0.7dev

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='z3c.relationfield',
-    version = '0.6',
+    version = '0.7dev',
     description="A relation field framework for Zope 3.",
     long_description=long_description,
     classifiers=[],
